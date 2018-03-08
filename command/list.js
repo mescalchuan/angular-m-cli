84ccdfb6351e94cee43d9931e1172cb2373221b2
Mescal Chuan <1335382915@qq.com>
2018-03-08 10:23:20 +0800

    Update list.js

--- command/list.js
+++ command/list.js
@@ -4,14 +4,14 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 module.exports = () => {
-	co(function* () {
-		fs.readdir('./pages', (err, data) => {
-			if(err) {
-				console.log(chalk.red('\n Can not find pages, please make sure that you are in the root of the project'))
-				process.exit();
-			}
-			console.log(data);
-			process.exit();
-		})
+    co(function* () {
+        fs.readdir('./pages', (err, data) => {
+	    if(err) {
+	        console.log(chalk.red('\n Can not find pages, please make sure that you are in the root of the project'))
+		process.exit();
+	    }
+	    console.log(data);
+	    process.exit();
 	})
+    })
 }
