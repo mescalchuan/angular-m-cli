80b62d3bbe99a4697790bded7f970cf33466e016
Mescal Chuan <1335382915@qq.com>
2018-03-08 10:21:41 +0800

    Update delete.js

--- command/delete.js
+++ command/delete.js
@@ -6,41 +6,41 @@ const rm = require('rimraf').sync;
 const prompt = require('co-prompt');
 
 module.exports = (pageName) => {
-	co(function* () {
-		const containerName = pageName;
+    co(function* () {
+	const containerName = pageName;
 
-	    console.log(chalk.yellow(`\n Your './entry/${containerName}, './pages/${containerName}', './css/${containerName}.scss' will be removed.`));
-	    const answer = yield prompt('\n are you sure? Input y or n \n');
+	console.log(chalk.yellow(`\n Your './entry/${containerName}, './pages/${containerName}', './css/${containerName}.scss' will be removed.`));
+	const answer = yield prompt('\n are you sure? Input y or n \n');
 	    
-	    if(answer == 'n' || !answer) process.exit();
+	if(answer == 'n' || !answer) process.exit();
 	    
-	    const destinationEntry = `./entry/${containerName}`;
-	    const destinationPage = `./pages/${containerName}`;
-	    const destinationScss = `./css/${containerName}.scss`;
+	const destinationEntry = `./entry/${containerName}`;
+	const destinationPage = `./pages/${containerName}`;
+	const destinationScss = `./css/${containerName}.scss`;
 
-	    try {
-	    	fs.readdirSync(destinationEntry);
-	    	fs.readdirSync(destinationPage);
-	    	fs.readFileSync(destinationScss);
-	    }
-	    catch(err) {
-	    	console.log(chalk.red(`\n Can not find ${containerName}.`));
-	    	process.exit();
-	    }
-
-	    const spinner = ora(`Deleting ${containerName} pages...`).start();
-	    try {
-	    	rm(destinationEntry);
-	    	rm(destinationPage);
-	    	rm(destinationScss);
-	    }
-	    catch(err) {
-	    	spinner.stop();
-			console.log(chalk.red(`\n Can not delete ${containerName}`));
-			process.exit();
-	    }
-	    spinner.stop();
-	    console.log(chalk.green('\n Delete succeed!'));
+	try {
+	    fs.readdirSync(destinationEntry);
+	    fs.readdirSync(destinationPage);
+	    fs.readFileSync(destinationScss);
+	}
+	catch(err) {
+	    console.log(chalk.red(`\n Can not find ${containerName}.`));
 	    process.exit();
-	})
+	}
+
+	const spinner = ora(`Deleting ${containerName} pages...`).start();
+	try {
+	    rm(destinationEntry);
+	    rm(destinationPage);
+	    rm(destinationScss);
+	}
+	catch(err) {
+	    spinner.stop();
+	    console.log(chalk.red(`\n Can not delete ${containerName}`));
+	    process.exit();
+	}
+	spinner.stop();
+	console.log(chalk.green('\n Delete succeed!'));
+	process.exit();
+    })
 }
