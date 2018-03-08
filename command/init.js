156d716d3f978f937d9f29b0aacc4c9db611a30c
Mescal Chuan <1335382915@qq.com>
2018-03-08 10:22:36 +0800

    Update init.js

--- command/init.js
+++ command/init.js
@@ -8,17 +8,17 @@ const rm = require('rimraf').sync;
 
 module.exports = (projectName) => {
     co(function*() {
-          console.log(chalk.white('\n Start generating...'));
-          const spinner = ora('Start downloading templates...').start();
-          if (exists(projectName)) rm(projectName);
-          download('1335382915/angular-custom-cli', projectName, (err) => {
-              if(err) {
-                  console.log(chalk.red(err));
-                  process.exit();
-              }
-              spinner.stop();
-              console.log(chalk.green('\n Download succeed! Enter your project and use `npm install`'));
-              process.exit();
-          })
+        console.log(chalk.white('\n Start generating...'));
+        const spinner = ora('Start downloading templates...').start();
+        if (exists(projectName)) rm(projectName);
+        download('1335382915/angular-custom-cli', projectName, (err) => {
+            if(err) {
+                console.log(chalk.red(err));
+                process.exit();
+            }
+            spinner.stop();
+            console.log(chalk.green('\n Download succeed! Enter your project and use `npm install`'));
+            process.exit();
+        })
     })
 }
